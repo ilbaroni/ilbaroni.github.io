927c5e8878dd60bfca602ba1b4544a2c36b63198
ilbaroni <ilbaroni@users.noreply.github.com>
2024-10-06 13:50:19 +0100

    update count.js

--- count.js
+++ count.js
@@ -1,6 +1,5 @@
 // GoatCounter: https://www.goatcounter.com
-// This file (and *only* this file) is released under the ISC license:
-// https://opensource.org/licenses/ISC
+// This file is released under the ISC license: https://opensource.org/licenses/ISC
 ;(function() {
 	'use strict';
 
@@ -113,7 +112,7 @@
 
 	// Filter some requests that we (probably) don't want to count.
 	goatcounter.filter = function() {
-		if ('visibilityState' in document && (document.visibilityState === 'prerender' || document.visibilityState === 'hidden'))
+		if ('visibilityState' in document && document.visibilityState === 'prerender')
 			return 'visibilityState'
 		if (!goatcounter.allow_frame && location !== parent.location)
 			return 'frame'
@@ -145,21 +144,27 @@
 		var f = goatcounter.filter()
 		if (f)
 			return warn('not counting because of: ' + f)
-
 		var url = goatcounter.url(vars)
 		if (!url)
 			return warn('not counting because path callback returned null')
 
-		var img = document.createElement('img')
-		img.src = url
-		img.style.position = 'absolute'  // Affect layout less.
-		img.setAttribute('alt', '')
-		img.setAttribute('aria-hidden', 'true')
+		if (!navigator.sendBeacon(url)) {
+			// This mostly fails due to being blocked by CSP; try again with an
+			// image-based fallback.
+			var img = document.createElement('img')
+			img.src = url
+			img.style.position = 'absolute'  // Affect layout less.
+			img.style.bottom = '0px'
+			img.style.width = '1px'
+			img.style.height = '1px'
+			img.loading = 'eager'
+			img.setAttribute('alt', '')
+			img.setAttribute('aria-hidden', 'true')
 
-		var rm = function() { if (img && img.parentNode) img.parentNode.removeChild(img) }
-		setTimeout(rm, 10000)  // In case the onload isn't triggered.
-		img.addEventListener('load', rm, false)
-		document.body.appendChild(img)
+			var rm = function() { if (img && img.parentNode) img.parentNode.removeChild(img) }
+			img.addEventListener('load', rm, false)
+			document.body.appendChild(img)
+		}
 	}
 
 	// Get a query parameter.
@@ -245,7 +250,21 @@
 
 	if (!goatcounter.no_onload)
 		on_load(function() {
-			goatcounter.count()
+			// 1. Page is visible, count request.
+			// 2. Page is not yet visible; wait until it switches to 'visible' and count.
+			// See #487
+			if (!('visibilityState' in document) || document.visibilityState === 'visible')
+				goatcounter.count()
+			else {
+				var f = function(e) {
+					if (document.visibilityState !== 'visible')
+						return
+					document.removeEventListener('visibilitychange', f)
+					goatcounter.count()
+				}
+				document.addEventListener('visibilitychange', f)
+			}
+
 			if (!goatcounter.no_events)
 				goatcounter.bind_events()
 		})
